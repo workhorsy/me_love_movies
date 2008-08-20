bb39926eeb84cfed105dd7a1c26b2abdeb48f2d1
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-08-19 18:58:32 -0700

    Changed the reviews so they will have to be less than a specific length. Added a little notification that dynamically tells the use how long the text is.

--- app/models/title_review.rb
+++ app/models/title_review.rb
@@ -2,5 +2,10 @@ class TitleReview < ActiveRecord::Base
 	belongs_to :title
 	belongs_to :user
 
+	def self.max_body_length
+		1000
+	end
+
 	validates_presence_of :user, :title, :body
+	validates_length_of :body, :in => 2..TitleReview::max_body_length, :allow_blank => :true
 end
