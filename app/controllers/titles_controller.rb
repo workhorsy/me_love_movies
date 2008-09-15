a119800f080c09b65e06912f1253c54930fc5d09
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-09-15 16:02:47 -0700

    Added basic search functionality for titles, directors, and actors.

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -121,31 +121,66 @@ class TitlesController < ApplicationController
 		# Just return unless this is the post back from the search button
 		return unless request.post?
 
-		# Make sure something was selected
-		if params[:title_rating].values.uniq == ["0"]
-			respond_to do |format|
-				flash[:notice] = "No search parameters were selected"
-				format.html { render :action => "search" }
-				format.xml	{ render :xml => @title_rating.errors, :status => :unprocessable_entity }
+		if params[:type] == 'by_name'
+			# Make sure something was selected
+			s_name = params[:name].strip
+			s_actor = params[:actor].strip
+			s_director = params[:director].strip
+			if [s_name, s_actor, s_director].uniq == ['']
+				respond_to do |format|
+					flash[:notice] = "No search parameters were selected"
+					format.html { render :action => "search" }
+					format.xml	{ render :xml => @title_rating.errors, :status => :unprocessable_entity }
+				end
+				return
+			else
+				flash[:notice] = nil
 			end
-			return
-		else
-			flash[:notice] = nil
+
+			# Do the search by category
+			db_field, search_params = nil, nil
+			if s_name != ''
+				db_field, search_params = 'name', s_name
+			elsif s_actor != ''
+				db_field, search_params = 'cast', s_actor
+			elsif s_director != ''
+				db_field, search_params = 'director', s_director
+			end
+
+			search_params = search_params.split(' ').collect { |n| n.strip }
+
+			@titles = Title.find(:all, :conditions => [
+											search_params.collect { |n| "#{db_field} like ?"}.join(' and '),
+											*search_params.collect { |n| "%#{n}%"}],
+								 :order => :name)
+
+		elsif params[:type] == 'by_rating'
+			# Make sure something was selected
+			if params[:title_rating].values.uniq == ["0"]
+				respond_to do |format|
+					flash[:notice] = "No search parameters were selected"
+					format.html { render :action => "search" }
+					format.xml	{ render :xml => @title_rating.errors, :status => :unprocessable_entity }
+				end
+				return
+			else
+				flash[:notice] = nil
+			end
+
+			# Generate a rating object from our search
+			@title_rating = TitleRating.new(params[:title_rating])
+
+			# Get only the fields that were not blank
+			fields = (Title::genres + Title::attributes)
+			selected_fields = fields.select { |f| @title_rating.send(f) }
+
+			# Find the titles that have matching field values. Order by most matching.
+			@titles = Title.find(:all, :conditions => [
+														selected_fields.collect { |f| "avg_#{f}=?" }.join(' or '),
+														*selected_fields.collect { |f| @title_rating.send(f) }
+														],
+									:order => selected_fields.collect { |f| "avg_#{f}" }.join(', ')).reverse
 		end
-
-		# Generate a rating object from our search
-		@title_rating = TitleRating.new(params[:title_rating])
-
-		# Get only the fields that were not blank
-		fields = (Title::genres + Title::attributes)
-		selected_fields = fields.select { |f| @title_rating.send(f) }
-
-		# Find the titles that have matching field values. Order by most matching.
-		@titles = Title.find(:all, :conditions => [
-													selected_fields.collect { |f| "avg_#{f}=?" }.join(' or '),
-													*selected_fields.collect { |f| @title_rating.send(f) }
-													],
-								:order => selected_fields.collect { |f| "avg_#{f}" }.join(', ')).reverse
 	end
 end
 
