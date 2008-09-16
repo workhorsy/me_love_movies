5405c816ba7eca6336e3fc541bbcb531589d34a8
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-09-16 00:43:55 -0700

    Added theme images, css files, and some html changes to the default layout. Specific page changes not implemented yet.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users, :member => { :set_user_type => :post,
 									:set_review_rating => :post },
 						:collection => { :login => :get, :list => :get }
-  map.resources :home
+  map.resources :home, :collection => { :index => :get }
   map.resources :titles, :collection => { :search => :get }
   map.resources :title_ratings
   map.resources :title_reviews
