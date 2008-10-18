17650d3b26031bd78c73e80645c0b4124ca0637a
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-17 17:36:35 -0700

    Changed the poster size to be 300px wide, and the link back to allposters.com to be more explanatory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,13 +42,13 @@ EOF
 			item_id, poster_url = posters[rand(posters.length)].split(',')
 <<EOF
 	<a class="APCTitleAnchor" href="http://affiliates.allposters.com/link/redirect.asp?item=#{item_id}&AID=#{Settings.affiliate_id}&PSTID=1&LTID=2&lang=1" target="_blank" title="#{title.proper_name}">
-		<img src="#{poster_url}" alt="#{title.proper_name}" border="0" height="" width="">
+		<img src="#{poster_url}" alt="#{title.proper_name}" border="0" height="300" width="">
 	</a>
 	<img src="http://tracking.allposters.com/allposters.gif?AID=#{Settings.affiliate_id}&PSTID=1&LTID=2&lang=1" border="0" height="1" width="1">
 	<br />
 	<span style="font-family:verdana,arial,helvetica;font-size:10;" >
 		<a class="APCTitleAnchor" href="http://affiliates.allposters.com/link/redirect.asp?item=#{item_id}&AID=#{Settings.affiliate_id}&PSTID=1&LTID=2&lang=1" target="_blank" title="#{title.proper_name}">
-			Buy at AllPosters.com
+			Buy this poster at AllPosters.com
 		</a>
 		<br />
 	</span>
