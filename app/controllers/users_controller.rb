03cfa32b7177a1a4685f72bbc1139f5e0277b6c9
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-23 22:01:05 -0700

    Changed the user greeting to be randomly selected from a list.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -399,9 +399,11 @@ class UsersController < ApplicationController
 	end
 
 	def login_set_sessions_and_cookies(user)
+		greetings = ['Howdy', 'Holla', 'Bonjour', 'Guten Tag', 'Aloha', 'Konnichi Wa']
 		session[:user_id] = user.id
 		cookies[:user_name] = { :value => user.user_name }
 		cookies[:user_greeting] = { :value => 'Howdy' }
+		cookies[:user_greeting] = { :value => greetings[rand(greetings.length)] }
 		cookies[:user_type] = { :value => user.user_type }
 		cookies[:user_id] = { :value => user.id.to_s }
 	end
