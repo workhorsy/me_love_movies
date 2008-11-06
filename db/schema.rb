ce87693216a40b197d6053f74210cb64f7e0aa1f
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-11-05 16:57:14 -0800

    Added some boiler plate tags.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20) do
+ActiveRecord::Schema.define(:version => 21) do
 
   create_table "ratings", :force => true do |t|
     t.string "name"
