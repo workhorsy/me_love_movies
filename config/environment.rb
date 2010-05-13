377caeae7b45216a643af20c8f3a2cc164371d33
Matthew Brennan Jones <mattjones@workhorsy.org>
2010-05-13 13:46:23 -0700

    Updated the site to work with rails 2.3.

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,13 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', 
-    :source => 'http://gems.github.com'
-
+  config.gem 'will_paginate'
   config.gem 'ezcrypto'
   config.gem 'mini_magick'
   # sudo apt-get install imagemagick
